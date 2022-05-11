94690c19b4d70bd478af9e71681596b885910d4c
Kushagra Ojha <86009160+Malwareman007@users.noreply.github.com>
2022-05-11 16:34:21 +0530

    Create instagramfinder.py

---
+++ Mod/instagramfinder.py
@@ -0,0 +1,11 @@
+from __future__ import print_function
+
+import os
+import sys
+from time import sleep
+
+from bs4 import BeautifulSoup
+from pyvirtualdisplay import Display
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+
