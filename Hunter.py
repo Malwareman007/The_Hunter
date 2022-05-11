1e1927af573748f3c48d88c899cdec135804df4e
Kushagra Ojha <86009160+Malwareman007@users.noreply.github.com>
2022-05-11 16:31:49 +0530

    Update Hunter.py

--- Hunter.py
+++ Hunter.py
@@ -1,1 +1,19 @@
-  
+from __future__ import print_function
+
+import argparse
+import csv
+import http.cookiejar
+import json
+import math
+import os
+import shutil
+import sys
+import traceback
+import urllib
+from datetime import datetime
+from shutil import copyfile
+
+import face_recognition
+import requests
+from bs4 import BeautifulSoup
+from django.utils import encoding
